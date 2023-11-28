42f50d8a9548bbac3d7802a5cd592e63a881d562
vladi <vschechurov@edu.hse.ru>
2023-11-28 12:29:25 +0300

    1

--- src/main/kotlin/com/example/cheat_project/HelloApplication.kt
+++ src/main/kotlin/com/example/cheat_project/HelloApplication.kt
@@ -1,17 +1,20 @@
 package com.example.cheat_project
 
 import javafx.application.Application
+import javafx.collections.FXCollections
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
+import javafx.scene.control.ChoiceBox
 import javafx.stage.Stage
 
 class HelloApplication : Application() {
     override fun start(stage: Stage) {
-        val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hello-view.fxml"))
-        val scene = Scene(fxmlLoader.load(), 320.0, 240.0)
+        var fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hello-view.fxml"))
+        var scene = Scene(fxmlLoader.load(), 800.0, 600.0)
         stage.title = "Hello!"
         stage.scene = scene
         stage.show()
+        stage.centerOnScreen()
     }
 }
 
--- src/main/kotlin/com/example/cheat_project/HelloController.kt
+++ src/main/kotlin/com/example/cheat_project/HelloController.kt
@@ -1,14 +1,57 @@
 package com.example.cheat_project
 
 import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.ChoiceBox
 import javafx.scene.control.Label
+import javafx.scene.control.TextField
+import javafx.stage.Stage
 
 class HelloController {
+
+    lateinit var codeInsertButton: Button
+    lateinit var codeGenButton: Button
+    @FXML
+    private lateinit var codeInsertText: TextField
+    @FXML
+    private lateinit var codeGenText: TextField
     @FXML
     private lateinit var welcomeText: Label
+    @FXML
+    private lateinit var nextButton: Button
+    @FXML
+    private lateinit var choiceBox: ChoiceBox<String>
 
     @FXML
     private fun onHelloButtonClick() {
-        welcomeText.text = "Welcome to JavaFX Application!"
+        nextButton.scene.window.hide()
+        val fxmlLoader : FXMLLoader
+        if (choiceBox.value == "Отправитель") {
+            fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("generate-code.fxml"))
+        } else {
+            fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("input-code.fxml"))
+        }
+        var scene = Scene(fxmlLoader.load(), 800.0, 600.0)
+        var stage = Stage()
+        stage.scene = scene
+        stage.show()
+    }
+
+    @FXML
+    private fun choiceBoxClick() {
+        nextButton.isDisable = false;
+    }
+
+    @FXML
+    private fun goBack() {
+        nextButton.scene.window.hide()
+        val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hellow-view.fxml"))
+    }
+
+    @FXML
+    private fun generateCode() {
+
     }
 }
