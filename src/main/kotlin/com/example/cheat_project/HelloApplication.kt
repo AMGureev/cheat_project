79a18310929b85e82d5da3687a6fabc04306f5f7
gnome-gnomych <gyrei_am@mail.ru>
2023-12-06 12:56:30 +0300

    many, many changes add a lot of fun()

--- src/main/kotlin/com/example/cheat_project/HelloApplication.kt
+++ src/main/kotlin/com/example/cheat_project/HelloApplication.kt
@@ -6,8 +6,16 @@ import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.scene.control.ChoiceBox
 import javafx.stage.Stage
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
+
 
 class HelloApplication : Application() {
+    private var thread : Thread? = null
     override fun start(stage: Stage) {
         var fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hello-view.fxml"))
         var scene = Scene(fxmlLoader.load(), 800.0, 600.0)
@@ -15,7 +23,40 @@ class HelloApplication : Application() {
         stage.scene = scene
         stage.show()
         stage.centerOnScreen()
+        startThread()
     }
+
+    private val executor = Executors.newSingleThreadScheduledExecutor()
+
+    private fun startThread() {
+        executor.scheduleAtFixedRate(::clearOldCodeInDateBase, 0, 30, TimeUnit.MINUTES)
+    }
+    private fun clearOldCodeInDateBase() {
+        thread = Thread {
+            val current = LocalDateTime.now()
+            val formatted = DateTimeFormatter.ofPattern("dd/MM/yy:HH:mm:ss")
+            val filename = File("databaseCode.txt")
+            val bufferedReader = filename.bufferedReader()
+            val linesToKeep = mutableListOf<String>()
+
+            bufferedReader.useLines { lines ->
+                lines.forEach { line ->
+                    val keyAndTime = line.split(" ").toTypedArray()
+                    val endTime = keyAndTime.elementAt(1)
+                    if (LocalDateTime.parse(endTime, formatted) > current) {
+                        linesToKeep.add(line)
+                    }
+                }
+            }
+            // Write the lines that should be kept back to the file
+            filename.bufferedWriter().use { writer ->
+                linesToKeep.forEach { writer.write("$it\n") }
+            }
+        }
+        thread!!.start()
+        println("Очистка произведена!")
+    }
+
 }
 
 fun main() {
